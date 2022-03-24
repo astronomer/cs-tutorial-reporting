d2bc24564318aa3d57fe612a07a448b3977f9099
frankbreetz <frank.breetz@gmail.com>
2022-03-24 14:13:30 -0400

    remove top level varibles

--- dags/reporting-dag.py
+++ dags/reporting-dag.py
@@ -12,14 +12,7 @@ headers = {
     "Content-Type": "application/json",
     "Accept": "*/*",
 }
-AIRFLOW_USERNAME = "admin"
-AIRFLOW_PASSWORD = "admin"
-AIRFLOW_WEBSERVER_HOST = "webserver"
-AIRFLOW_WEBSERVER_PORT = "8080"
-GCP_CONN_ID = "google_cloud_storage"
-BUCKET = "customer-success-reporting"
-PG_CONN_ID = "my_postgres_conn_id"
-pg_hook = PostgresHook(postgres_conn_id=PG_CONN_ID)
+
 
 
 def get_existing_dag_info(ti, **kwargs):
@@ -60,18 +53,18 @@ with DAG(
         )
         t2 = AirflowToGCSOperator(
             task_id="dags_to_gcs",
-            bucket=BUCKET,
+            bucket="customer-success-reporting",
             airflow_object="dags",
-            gcp_conn_id=GCP_CONN_ID,
+            gcp_conn_id="google_cloud_storage",
             dst="airflow/dags/{{ ts_nodash }}/",
         )
         t3 = GCSToPostgres(
             task_id="dag_gcs_to_postgres",
-            bucket=BUCKET,
+            bucket="customer-success-reporting",
             destination_table="rpt.dag",
             source_format="JSON",
             source_objects="airflow/dags/{{ ts_nodash }}/dags.json",
-            google_cloud_storage_conn_id=GCP_CONN_ID,
+            google_cloud_storage_conn_id="google_cloud_storage",
             pg_conn_id=PG_CONN_ID,
             pk_col="dag_id",
             schema_fields=[
@@ -94,26 +87,26 @@ with DAG(
         )
         t5 = AirflowToGCSOperator(
             task_id="dag_runs_to_gcs",
-            bucket=BUCKET,
+            bucket="customer-success-reporting",
             batch_size=10000,
             airflow_object="dagRuns",
-            gcp_conn_id=GCP_CONN_ID,
+            gcp_conn_id="google_cloud_storage",
             last_upload_date="{{ti.xcom_pull(task_ids='dag_runs.set_max_dag_run_start', key='MAX_DAG_START')}}",
             dst="airflow/dag_runs/{{ ts_nodash }}/",
         )
         t6 = GCSListObjectsOperator(
             task_id="list_dag_run_objects",
-            bucket=BUCKET,
-            gcp_conn_id=GCP_CONN_ID,
+            bucket="customer-success-reporting",
+            gcp_conn_id="google_cloud_storage",
             prefix="airflow/dag_runs/{{ ts_nodash }}/",
         )
         t7 = GCSToPostgres(
             task_id="dag_run_gcs_to_postgres",
-            bucket=BUCKET,
+            bucket="customer-success-reporting",
             destination_table="rpt.dag_run",
             source_format="JSON",
             source_objects=t6.output,
-            google_cloud_storage_conn_id=GCP_CONN_ID,
+            google_cloud_storage_conn_id="google_cloud_storage",
             pg_conn_id=PG_CONN_ID,
             schema_fields=[
                 "dag_id",
@@ -134,25 +127,25 @@ with DAG(
         )
         t9 = AirflowToGCSOperator(
             task_id="task_instance_to_gcs",
-            bucket=BUCKET,
+            bucket="customer-success-reporting",
             airflow_object="taskInstances",
-            gcp_conn_id=GCP_CONN_ID,
+            gcp_conn_id="google_cloud_storage",
             last_upload_date="{{ti.xcom_pull(task_ids='task_instances.set_max_task_instance', key='MAX_TASK_START')}}",
             dst="airflow/task_instance/{{ ts_nodash }}/",
         )
         t10 = GCSListObjectsOperator(
             task_id="list_task_instance_objects",
-            bucket=BUCKET,
-            gcp_conn_id=GCP_CONN_ID,
+            bucket="customer-success-reporting",
+            gcp_conn_id="google_cloud_storage",
             prefix="airflow/task_instance/{{ ts_nodash }}/",
         )
         t11 = GCSToPostgres(
             task_id="task_instance_gcs_to_postgres",
-            bucket=BUCKET,
+            bucket="customer-success-reporting",
             destination_table="rpt.task_instance",
             source_format="JSON",
             source_objects=t10.output,
-            google_cloud_storage_conn_id=GCP_CONN_ID,
+            google_cloud_storage_conn_id="google_cloud_storage",
             pg_conn_id=PG_CONN_ID,
             schema_fields=[
                 "dag_id",
